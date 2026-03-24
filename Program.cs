059a8ea567d34170e4a15bd55da1c982dcd8dcc4
papousek11 <89077005+papousek11@users.noreply.github.com>
2026-03-24 14:22:03 +0100

    zaklad soubory

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+
+
+
+
+namespace main;
+
+//předem se omlouvam že musíte známkovat tyhle špagety
+class EntryClass
+{
+
+
+
+    static void Main(string[] args)
+    {
+    }
+    
+}
